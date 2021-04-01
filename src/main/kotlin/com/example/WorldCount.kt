0a54c127e4cad7a5345e5b7d7132c772d6b1c968
santojos <santoshjoshi2003@yahoo.co.in>
2021-04-01 21:14:19 +0530

    Flink Kotlin Hello World Example

--- src/main/kotlin/com/example/WorldCount.kt
+++ src/main/kotlin/com/example/WorldCount.kt
@@ -1,25 +1,25 @@
 package com.example
 
-import org.apache.commons.compress.utils.Lists
 import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.api.java.ExecutionEnvironment
-import org.apache.flink.api.java.functions.FlatMapIterator
 import org.apache.flink.api.java.operators.DataSource
-import org.apache.flink.api.java.tuple.Tuple
 import org.apache.flink.api.java.tuple.Tuple2
 import org.apache.flink.util.Collector
-import java.lang.Exception
-
 
 fun main(args: Array<String>) {
 
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment()
-    var dataSource: DataSource<String> = env.readTextFile("/tmp/worldcount.txt")
+    var dataSource: DataSource<String> = env.readTextFile(args.get(0))
 
-
-    dataSource.flatMap( FlatMapFunction { value: String, out: Collector<Tuple2<String, Int>>  -> value.split(" ").iterator().forEach { out.collect(Tuple2(it, 1)) } })
+    dataSource.flatMap(
+        FlatMapFunction { value, out: Collector<Tuple2<String, Int>> ->
+            value.split(" ")
+                .iterator()
+                .forEach { out.collect(Tuple2(it, 1)) }
+        })
         .groupBy(0)
         .sum(1)
         .print()
 
+    print("Word Count !!")
 }
